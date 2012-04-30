487fada5590a25bc6c4790df02545e28c98ca8cf
vit <info@tzcon.com>
2012-04-30 17:27:58 +0400

    Таймауты и тесты

--- rupost.php
+++ rupost.php
@@ -13,16 +13,15 @@ $param=array();
 $url='http://www.russianpost.ru/resp_engine.aspx?Path=rp/servise/ru/home/postuslug/trackingpo';
 //download ruspost for parsing params 
 $curl_opt=array(CURLOPT_URL=>$url,
-				CURLOPT_RETURNTRANSFER=>true,
+				CURLOPT_RETURNTRANSFER=>1,
 				CURLOPT_HEADER=>0,
 				CURLOPT_FOLLOWLOCATION=>1,
-				CURLOPT_CONNECTTIMEOUT=>30,
-				CURLOPT_VERBOSE=>30,
+				CURLOPT_CONNECTTIMEOUT=>10,				
 				CURLOPT_USERAGENT=>'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.8.1)');
 $ch=curl_init();
 $html = new simple_html_dom();
 curl_setopt_array($ch,$curl_opt);
-$html_str=curl_exec($ch);
+if (!($html_str=curl_exec($ch))) timeout_error();  
 $html=str_get_html($html_str);
 //$html = file_get_html('./ruspost/ruspost.html');		
 $form = $html->find("form",0);  
@@ -36,8 +35,9 @@ $param[]='entryBarCode=';
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS,implode('&',$param));
 curl_setopt($ch, CURLOPT_REFERER, $url);		
-$html_str = curl_exec($ch);
+if (!($html_str=curl_exec($ch))) timeout_error(); 
 curl_close($ch); 
+
 $html = str_get_html($html_str);
 if (($table1 = $html->find("#PRINTBODY table",2)) && ($table2 = $html->find("#PRINTBODY table",3)))
 {
@@ -45,7 +45,7 @@ if (($table1 = $html->find("#PRINTBODY table",2)) && ($table2 = $html->find("#PR
 	foreach ($table1->children() as $tr)
 	 foreach ($tr->children() as $th)
 	  $th->style="border-bottom:1px solid #ccc;";	  
-	$table1->innertext='<tr valign="top"><th align="left"  style="border-bottom:1px solid #ccc;">Источник информации:</th><td  style="border-bottom:1px solid #ccc;"><a href="'.$url.'" target="_blank">Почта России</a></td></tr>'.$table1->innertext;   
+	$table1->innertext='<tr valign="top"><th align="left"  style="border-bottom:1px solid #ccc;">Источник информации:</th><td  style="border-bottom:1px solid #ccc;"><a href="'.$url.'" target="_blank">Почта России</a></td></tr>'.$table1->innertext;   	
 	echo $table1."\n";
 	$table2->bgcolor='white';
 	$table2->align='center';
@@ -55,9 +55,19 @@ if (($table1 = $html->find("#PRINTBODY table",2)) && ($table2 = $html->find("#PR
 	$table2=preg_replace("|class=\"[^\"]*?\"|","",$table2);
 	echo $table2."\n";
 }
-else 
-	echo "<center><h3><font color=red>К сожалению, информация о почтовом отправлении с номером {$_GET['pid']} не найдена.</font></h3></center>";	
+else if ($err = $html->find(".red",0)->innertext)	
+	echo "<center><h4><font color=red>$err</font></h4></center>";	
+else 	
+	echo"<center><h4><font color=red>Не удалось получить информацию о почтовом отправлении <b>{$_GET['pid']}</b>, возможно изменился формат вывода данных.</font><br>
+	Источник информации:<a href=\"'.$url.'\" target=\"_blank\">Почта России</a></h4></center>";	
 $html->clear;  
 ?>
 </body>
 </html>
+<?php
+function timeout_error()
+{
+  global $url;
+  die("<center><h4><font color=red>Сайт <a href=\"'.$url.'\" target=\"_blank\">Почты России</a> не ответил вовремя. Попробуйте повторить запрос позднее.</font></h4></center></body></html>");
+ }
+?>
